5ae3e6dea54c79bd2ee78f2d923ba588172311fc
Debarchhana Hota <hotadebarchhana@gmail.com>
2026-06-08 22:21:01 +0530

    update

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-console.log("Hello JenkinasbPipeloine");
-let a=5;
-let b=25;
-console.log("total value is: ",a+b);
-let c = 50
+const http = require('http');
+const server = http.createServer((req, res) => {
+    res.write("Hello from Jenkins Auto Deplyment");
+    res.end();
+});
+server.listen(3000, '0.0.0.0', () => {
+    console.log("Server running on port 3000");
+});
