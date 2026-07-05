f7ac39d7aea80302d37e5e8cd4e5a5e63b245b5c
Debarchhana Hota <hotadebarchhana@gmail.com>
2026-07-06 00:20:29 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,9 @@
+//console.log("Hello from docker");
+const express = require("express");
+const app = express();
+app.get("/", (req, res) => {
+  res.send("Hello Docker");
+});
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
